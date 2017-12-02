9432db18704ff6c0b0cd94adb04814702ee48a8f
lonardonetto <webvendedor@yahoo.com.br>
2017-12-02 00:21:16 -0200

    Add files via upload

---
+++ Email.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Notification;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+class Email
+{
+    private $mail = \stdClass::class;
+
+    public function __construct()
+    {
+        $this->mail = new PHPMailer(true);                  // Passing `true` enables exceptions
+        $this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $this->mail->isSMTP();                                      // Set mailer to use SMTP
+        $this->mail->Host = 'smtp.live.com';  // Specify main and backup SMTP servers
+        $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $this->mail->Username = 'contato.lvg@hotmail.com';                 // SMTP username
+        $this->mail->Password = 'Lvg@2015';                           // SMTP password
+        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = 587;                                    // TCP port to connect to
+        $this->mail->CharSet = "UTF-8";
+        $this->mail->setLanguage('br');
+        $this->mail->isHTML(true);
+        $this->mail->setFrom('contato.lvg@hotmail.com', 'LVG Import && Export');
+
+    }
+
+    public function sendMail($subject, $body, $replyEmail,$replyName, $addressEmail, $addressName)
+    {
+        $this->mail->Subject = (string)$subject;
+        $this->mail->Body = $body;
+
+        $this->mail->addReplyTo($replyEmail, $replyName);
+        $this->mail->addAddress($addressEmail, $addressName);
+
+        try{
+            $this->mail->send();
+        }catch(Exception $e){
+             echo "Erro ao enviar email: {$this->mail->ErrorInfo} {$e->getMessage()}";
+        }
+    }
+}
